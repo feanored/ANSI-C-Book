ef982e73ce17659c8cf4f623401955d75e740dac
feanored <eduardo.floyd@outlook.com>
2025-12-06 10:23:04 -0300

    Aproximando PI

--- src/UNION1.C
+++ src/UNION1.C
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define _USE_MATH_DEFINES 1
+#include <math.h>
 
 typedef union jx {
 	int a;
@@ -11,10 +13,12 @@ int main() {
 
 	x.a = 42;
 	x.b = 0x42;
-	x.c = 355.0 / 113;
+	x.c = 104348. / 33215;
 	printf("a: %d \n", x.a);
 	printf("b: %d \n", x.b);
-	printf("c: %.15f \n", x.c);
+	printf("c:    %.12f \n", x.c);
+	printf("Pi45: %.12f \n", atan(1) * 4);
+	printf("M_Pi: %.12f \n", M_PI);
 
 	return 0;
 }
