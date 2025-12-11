d28ef56dcab634965506a98542b183946448a66a
feanored <eduardo.floyd@outlook.com>
2025-12-11 09:05:12 -0300

    Cpu Util

---
+++ src/cpu.c
@@ -0,0 +1,22 @@
+// Check if CPU if Big Endian or Litle Endian
+
+#include <stdio.h>
+#include <stdint.h>
+
+#ifndef int32_t
+#define int32_t long
+#endif
+
+int main(int argc, char** argv) {
+	char	bytearray[4] = { 0x01,0x02,0x03,0x04 };
+	long* b = (int32_t*)bytearray;
+
+	printf("%d", *b);
+	if (*b == 0x01020304) {
+		printf("\nBig Endian!\n");
+	}
+	else {
+		printf("\nLitle Endian!\n");
+	}
+	return	0;
+}
