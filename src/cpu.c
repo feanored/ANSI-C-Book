1ce355aa3b6f192cd0aec62a4d6b1791292b526c
Eduardo Galvani Massino <eduardo.floyd@outlook.com>
2025-12-20 22:59:54 -0300

    Update cpu.c

--- src/cpu.c
+++ src/cpu.c
@@ -1,22 +1,18 @@
-// Check if CPU if Big Endian or Litle Endian
-
+// Check if CPU is Big Endian or Little Endian
 #include <stdio.h>
-#include <stdint.h>
-
-#ifndef int32_t
-#define int32_t long
-#endif
 
 int main() {
-	char	bytearray[4] = { 0x01,0x02,0x03,0x04 };
-	long* b = (int32_t*)bytearray;
+  char bytearray[4] = { 0x41,0x42,0x43,0x44 };
+  int *b = (int*)bytearray;
 
-	printf("%d", *b);
-	if (*b == 0x01020304) {
-		printf("\nBig Endian!\n");
-	}
-	else {
-		printf("\nLitle Endian!\n");
-	}
-	return	0;
+  printf("%s <> %d\n", bytearray, *b);
+  printf("%d <> %d\n", 0x41424344, 0x44434241);
+
+  if (*b == 0x41424344) {
+    printf("Big Endian!\n");
+  }
+  else if (*b == 0x44434241) {
+    printf("Little Endian!\n");
+  }
+  return 0;
 }
